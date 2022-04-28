d11c8032af8d8caa4b30ae78a21d43da6490a2e0
gjgustjd <gjgustjd70@naver.com>
2022-04-29 02:10:38 +0900

    :art:Style:Hilt 관련 Annotation 적용
    ShowCurrencyApplication.kt 추가 및 HiltAndroidApp 어노테이션 추가
    AndroidManifest.xml에 ShowCurrencyApplication 설정
    MainActivity.kt AndroidEntryPoint 설정

--- app/src/main/java/com/example/showcurrencyapplication/activity/MainActivity.kt
+++ app/src/main/java/com/example/showcurrencyapplication/activity/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.example.showcurrencyapplication
+package com.example.showcurrencyapplication.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.showcurrencyapplication.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/showcurrencyapplication/common/ShowCurrencyApplication.kt
@@ -0,0 +1,8 @@
+package com.example.showcurrencyapplication.common
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ShowCurrencyApplication: Application() {
+}
